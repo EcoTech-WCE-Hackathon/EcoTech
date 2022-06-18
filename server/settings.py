0f151c5a84c207b19f8f3fa6a6700b4ae913732b
Burhanuddin Merchant <bmerchant945@gmail.com>
2022-06-18 19:58:35 +0530

    added api versioning and fixed dev run issue with psycopg2 cause by django-heroku

--- server/settings.py
+++ server/settings.py
@@ -13,7 +13,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-import django_heroku
+
+if os.getenv("PROD", "True") == "True":
+    import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -177,4 +180,6 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-django_heroku.settings(locals())
+
+if os.getenv("PROD", "True") == "True":
+    django_heroku.settings(locals())
--- server/urls.py
+++ server/urls.py
@@ -14,17 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("api/auth/", include("authentication.urls")),
-    path("api/dashboard/", include("dashboard.urls")),
-    path("api/mobile/", include("mobile.urls")),
-    path("api/recycler/", include("recycler.urls")),
+    path("admin/", admin.site.urls),
+    path("api/v1/auth/", include("authentication.urls")),
+    path("api/v1/dashboard/", include("dashboard.urls")),
+    path("api/v1/mobile/", include("mobile.urls")),
+    path("api/v1/recycler/", include("recycler.urls")),
 ]
 
 if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
