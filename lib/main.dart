36457846e728e00730a99a180674076eb726c96b
RohinBhat <bhatrohin003@gmail.com>
2022-06-20 19:46:05 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:ecotech/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
