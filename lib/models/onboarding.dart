6af9f723bca979416110228a121b7781ad444546
RohinBhat <bhatrohin003@gmail.com>
2022-06-22 22:23:34 +0530

    Fixed Image path

--- lib/models/onboarding.dart
+++ lib/models/onboarding.dart
@@ -7,11 +7,11 @@ class Onboarding {
 
 List<Onboarding> onboardingContent = [
   Onboarding(
-    image: '../../images/ecotech-1.png',
+    image: 'images/ecotech-1.png',
     description: "The one-stop e-waste management solution.",
   ),
   Onboarding(
-    image: '../../images/ecotech-2.png',
+    image: 'images/ecotech-2.png',
     description: "Report e-waste and earn rewards.",
   ),
 ];
